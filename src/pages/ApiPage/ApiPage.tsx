a2d79b314f68b236be43e4668bc4882752d2d5e7
Alexandr Kletnyy <q@obscur.space>
2023-12-21 00:02:44 +0300

    fix: response type

--- src/pages/ApiPage/ApiPage.tsx
+++ src/pages/ApiPage/ApiPage.tsx
@@ -2,12 +2,12 @@ import {useEffect, useState} from "react";
 import {Spin} from "antd";
 import TypeDoc from "@/components/TypeDoc/TypeDoc.tsx";
 import {getDocs} from "@/api";
+import type {ApiDoc} from "@/components/TypeDoc/api-docs.types.ts";
 
 const ApiPage = () => {
-  const [apiJson, setApiJson ] = useState();
+  const [apiJson, setApiJson ] = useState<ApiDoc>();
 
   useEffect(() => {
-    // @ts-ignore
     getDocs.request().getResult().then(({ data }) => {
       setApiJson(data);
     })
